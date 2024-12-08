ef638438e7cb75d1ddaece5d51a8e356172f6046
Afifa Sadiq <152473215+AfifaSadiq@users.noreply.github.com>
2024-12-08 13:49:57 +0530

    Update BruteForce.java

--- MergeIntervals/BruteForce.java
+++ MergeIntervals/BruteForce.java
@@ -1,34 +1,28 @@
-import java.util.*;
-
 class Solution {
     public int[][] merge(int[][] arr) {
-        int n = arr.length; // size of the array
-        // Sort the given intervals:
-        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        int len = arr.length;
+        Arrays.sort(arr, (a,b) -> Integer.compare(a[0], b[0]));
 
         List<int[]> ans = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) { // Select an interval:
+        for(int i = 0; i < len; i++) {
             int start = arr[i][0];
             int end = arr[i][1];
+            
+            //if that inerval is already included in the solution then skipped
+            if(!ans.isEmpty() && end <= ans.get(ans.size()-1)[1]) continue;
 
-            // Skip all the merged intervals:
-            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1)[1]) {
-                continue;
-            }
-
-            // Check the rest of the intervals:
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j][0] <= end) {
+            for(int j=i+1; j<len; j++) {
+                if(arr[j][0] <= end) {
                     end = Math.max(end, arr[j][1]);
-                } else {
+                }
+                else {
                     break;
                 }
             }
+
             ans.add(new int[]{start, end});
         }
 
-        // Convert the List<int[]> to int[][]:
         return ans.toArray(new int[ans.size()][]);
     }
 }
